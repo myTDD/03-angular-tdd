6f256464134117d72cb58528859a6d48c84f23f7
Pieter Siegers <pieter@coursetune.com>
2022-02-17 21:15:10 -0600

    changes for initial app for level II

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewMessageFormComponent } from './src/components/new-message-form/new-message-form.component';
+import { MessageListComponent } from './src/components/message-list/message-list.component';
+import { HeaderComponent } from './src/components/header/header.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewMessageFormComponent,
+    MessageListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
